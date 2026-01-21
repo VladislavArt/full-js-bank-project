7413d41c287118deeda31bdd31d795f48a1d5221
Vladislav Milrat <vrooog@gmail.com>
2026-01-21 10:17:39 +0300

    final version

---
+++ src/api/statistic.service.js
@@ -0,0 +1,13 @@
+import { redQuery } from "@/core/red-query/red-query.lib"
+
+
+export class StatisticService {
+	#BASE_URL = '/statistics'
+
+	main(onSuccess) {
+		return redQuery({
+			path: this.#BASE_URL,
+			onSuccess
+		})
+	}
+}
---
+++ src/core/services/validation.service.js
@@ -0,0 +1,26 @@
+const { COLORS } = require("@/config/colors.config")
+
+class ValidationService {
+	constructor() {
+		this.errorBorderTimeout = {}
+	}
+
+	showError(element, timeout = 2500) {
+		element.css('border-color', COLORS.error)
+
+		if (this.errorBorderTimeout[element]) {
+			clearTimeout(this.errorBorderTimeout[element])
+		}
+
+		this.errorBorderTimeout[element] = setTimeout(() => {
+			element.css('border-color', '')
+		}, timeout)
+		
+		this.errorBorderTimeout[element] = setTimeout(() => {
+			element.css('border-color', '')
+		}, timeout)
+
+	}
+}
+
+export default new ValidationService()
